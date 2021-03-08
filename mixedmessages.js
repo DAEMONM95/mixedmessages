a2d3aba78021887b15b9da5d19a44534edd765cf
Daniel Montoya <50853664+DAEMONM95@users.noreply.github.com>
2021-03-07 19:33:02 -0500

    Commit with random messages generated with each program execution

--- mixedmessages.js
+++ mixedmessages.js
@@ -1,1 +1,20 @@
-console.log('Hola gonorreaaaas!');
+// Arrays of phrase words
+let verbs, nouns, adjectives, adverbs, preposition;
+nouns = ["bird", "clock", "boy", "plastic bag", "duck", "teacher", "woman", "professor", "hamster", "dog"];
+verbs = ["kicked", "ran", "flew", "dodged", "sliced", "rolled", "died", "breathed", "slept", "killed"];
+adjectives = ["beautiful", "lazy", "professional", "lovely", "dumb", "rough", "soft", "hot", "vibrating", "slimy"];
+adverbs = ["slowly", "elegantly", "precisely", "quickly", "sadly", "humbly", "proudly", "shockingly", "calmly", "passionately"];
+preposition = ["down", "into", "up", "on", "upon", "below", "above", "through", "across", "towards"];
+// Loop will  iterate through each of these arrays in order to create a phrase - Random phrase selector
+let randSel = (wordArray) => {
+    let word = Math.floor(Math.random()*wordArray.length);
+    return wordArray[word]
+};
+
+let generatedSentence = () => {
+    //let sentence = "A "+ randSel(nouns) + " " + randSel(adverbs) + " " +  randSel(verbs);
+    let sentence = `A ${randSel(nouns)} ${randSel(adverbs)} ${randSel(verbs)} a ${randSel(adjectives)} ${randSel(nouns)} ${randSel(preposition)} the ${randSel(nouns)}`
+    console.log(sentence);
+};
+
+generatedSentence();
